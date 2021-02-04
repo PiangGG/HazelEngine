fe266a1c9878cdfbcd25a8e5c07b85e0a7e0d3e2
PiangGG <piangpiangpiang@outlook.com>
2021-02-04 21:25:25 +0800

    5

--- HazelEngine/src/Hazel/EntryPoint.h
+++ HazelEngine/src/Hazel/EntryPoint.h
@@ -6,7 +6,7 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc,char **argv)
 {
-	auto app = Hazel::CreateApplication();
+	auto app =  Hazel::CreateApplication();
 	app->Run();
 	delete app;
 }
