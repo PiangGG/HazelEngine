d7dce85c7301eddf0ac58b9efd51d3cb205e7415
PiangGG <piangpiangpiang@outlook.com>
2021-02-05 18:23:30 +0800

    06Logging Game Engine Series

--- HazelEngine/src/Hazel/EntryPoint.h
+++ HazelEngine/src/Hazel/EntryPoint.h
@@ -6,6 +6,11 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc,char **argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 5, b = 4;
+	HZ_CLIENT_INFO("Hello! var={0} var={1}",a,b);
+
 	auto app =  Hazel::CreateApplication();
 	app->Run();
 	delete app;
