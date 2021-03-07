4294b14367bb2db624efdb9c5b4aa7d721c3a006
PiangGG <piangpiangpiang@outlook.com>
2021-03-07 12:25:24 +0800

    30 渲染器API抽象

--- HazelEngine/src/Hazel/Application.h
+++ HazelEngine/src/Hazel/Application.h
@@ -9,6 +9,8 @@
 #include "ImGui/ImGuiLayer.h"
 
 #include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/Buffer.h"
+
 namespace Hazel {
 
 	//这样会自动产生 .lib文件和 .dll文件的
@@ -39,8 +41,10 @@ namespace Hazel {
 
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 	private:
 		static Application* s_Instance;
 	};
