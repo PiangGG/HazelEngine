5517a7830e05b0817b7eefe91052bdde1d88e13d
PiangGG <piangpiangpiang@outlook.com>
2021-03-15 13:29:46 +0800

    39 Material Systems

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -94,7 +94,7 @@ public :
 
 		m_Shader.reset(new Hazel::Shader(vectexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
@@ -111,19 +111,21 @@ public :
 			}
 		)";
 
-		std::string  blueShaderfragmentSrc = R"(
+		std::string  flatColorShaderfragmentSrc = R"(
 			#version 330 core
 
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
 		
+			uniform vec4 u_Color;
+			
 			void main()
 			{
-				color = vec4(0.2,0.3,0.8,1.0);
+				color = u_Color;
 			}
 		)";
-		m_BlueShader.reset(new Hazel::Shader(blueShaderVertexSrc, blueShaderfragmentSrc));
+		m_FlatColorShader.reset(new Hazel::Shader(flatColorShaderVertexSrc, flatColorShaderfragmentSrc));
 	}
 	void OnUpdate(Hazel::Timestep ts)override
 	{
@@ -154,13 +156,27 @@ public :
 		
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+
+		glm::vec4 redColor(0.8f, 0.2f, 0.3f, 1.0f);
+		glm::vec4 blueColor(0.2f, 0.3f, 0.8f, 1.0f);
+
+		Hazel::MaterialRef* material = new Material(m_FlatColorShader);
+		Hazel::MaterialInstanceRef* mi = new MaterialInstanceRef(material);
+
+		material->Set("u_Color", redColor);
+		squareMesh->SetMaterial(mi);
+
 		for (int y= 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++) 
 				{
 					glm::vec3 pos(x*0.11f, y * 0.11f,0.0f);
 					glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos)*scale;
-					Hazel::Renderer::Submit(m_BlueShader, m_SquareVA,transform);
+					/*if (x % 2 == 0)
+						m_FlatColorShader->UploadUniformFloat4("u_Color",redColor);
+					else
+						m_FlatColorShader->UploadUniformFloat4("u_Color",blueColor);*/
+					Hazel::Renderer::Submit(mi, m_SquareVA,transform);
 				}
 		}
 			
@@ -181,7 +197,7 @@ private:
 	std::shared_ptr<Hazel::Shader> m_Shader;
 	std::shared_ptr<Hazel::VertexArray> m_VertexArray;
 
-	std::shared_ptr<Hazel::Shader> m_BlueShader;
+	std::shared_ptr<Hazel::Shader> m_FlatColorShader;
 	std::shared_ptr<Hazel::VertexArray> m_SquareVA;
 
 	Hazel::OrthographicCamera m_Camera;
