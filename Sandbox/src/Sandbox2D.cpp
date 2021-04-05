a1405e8b29a8fa88454d364f55bdbe7999cd139a
PiangGG <piangpiangpiang@outlook.com>
2021-04-05 16:43:03 +0800

    55 Intro to Profiling

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -3,7 +3,47 @@
 
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <chrono>
 
+template<typename Fn>
+class Timer 
+{
+public:
+	Timer(const char* name, Fn&& func)
+		:m_Name(name),m_Func(func),m_Stopped(false)
+	{
+		m_StartTimepoint = std::chrono::high_resolution_clock::now();
+	}
+
+	~Timer() 
+	{
+		if (!m_Stopped)
+		{
+			Stop();
+		}
+	}
+
+	void Stop() 
+	{
+		auto endTimepoint = std::chrono::high_resolution_clock::now();
+
+		long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
+		long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
+		
+		m_Stopped = true;
+
+		float duration = (end - start) * 0.001f;
+		m_Func({m_Name,duration});
+	}
+	
+private:
+	const char* m_Name;
+	std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
+	bool m_Stopped;
+	Fn m_Func;
+};
+
+#define PROFILE_SCOPE(name) Timer timer##__LINE__(name, [&](ProfileResult profileResult) {m_ProfileResults.push_back(profileResult);})
 
 Sandbox2D::Sandbox2D()
 	:Layer("Sandbox2D"),m_CameraController(1280.0f/720.0f,true)
@@ -21,12 +61,21 @@ void Sandbox2D::OnDetach()
 
 void Sandbox2D::OnUpdate(Hazel::Timestep ts)
 {
+	PROFILE_SCOPE("Sandbox2D::OnUpdate");
 	//update
-	m_CameraController.OnUpdate(ts);
+	{
+		PROFILE_SCOPE("CameraController::OnUpdate");
+		m_CameraController.OnUpdate(ts);
+	}
+
 
 	//Render
-	Hazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f,1 });
-	Hazel::RenderCommand::Clear();
+	{
+		PROFILE_SCOPE("Renderer Prep");
+		Hazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f,1 });
+		Hazel::RenderCommand::Clear();
+	}
+	
 
 	Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
 	Hazel::Renderer2D::DrawQuad({ -1.0f,0.0f }, { 0.8f,0.8f }, {0.8f,0.2f,0.3f,1.0f});
@@ -39,6 +88,15 @@ void Sandbox2D::OnImGuiRender()
 {
 	ImGui::Begin("Settings");
 	ImGui::ColorEdit4("Square Color",glm::value_ptr(m_SquareColor));
+
+	for (auto& result:m_ProfileResults)
+	{
+		char lable[50];
+		strcpy(lable, "%.3fms ");
+		strcat(lable,result.Name);
+		ImGui::Text(lable,result.Time);
+	}
+	m_ProfileResults.clear();
 	ImGui::End();
 }
 
